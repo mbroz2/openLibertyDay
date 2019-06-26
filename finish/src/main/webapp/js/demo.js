8065821c0a08fd1a10f6c13499a6a787c1cbfb64
Michal Broz <mbroz@us.ibm.com>
2019-06-26 14:06:04 -0500

    Refactor js and add backend wrapper call

--- finish/src/main/webapp/js/demo.js
+++ finish/src/main/webapp/js/demo.js
@@ -1,4 +1,4 @@
-function getCoordinates() {
+function getCoordinatesAndCity() {
     document.getElementById("city").style.removeProperty("display");
     document.getElementById("city").innerHTML = "City: Retrieving.....";
     document.getElementById("coordinates").style.removeProperty("display");
@@ -15,11 +15,7 @@ function getCoordinates() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
-            var city = response.records[0].fields.city;
-            var coordinates = response.records[0].fields.geopoint;
-            document.getElementById("city").innerHTML = "City: " + city;
-            document.getElementById("coordinates").innerHTML = "Coordinates: "
-                + coordinates;
+            var coordinates = setCoordinatesAndCity(response);
             getStation(coordinates);
         } else if (this.readyState == 4 && this.status != 200) {
             document.getElementById("city").innerHTML = "City: Unavailable, try again later.";
@@ -38,10 +34,8 @@ function getStation(cooridnates) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
-            var cwa = response.properties.cwa;
+            setStation(response);
             var forecastURL = response.properties.forecast;
-            document.getElementById("cwa").innerHTML = "Center Weather Advisory: "
-                + cwa;
             getWeeklyForecast(forecastURL);
         } else if (this.readyState == 4 && this.status != 200) {
             document.getElementById("cwa").innerHTML = "Center Weather Advisory: Unavailable, try again later.";
@@ -56,40 +50,92 @@ function getWeeklyForecast(forecastURL) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            // Create forecast table
-            table = document.createElement('table');
-            trHeader = document.createElement('tr');
-            trTemp = document.createElement('tr');
-            trIcon = document.createElement('tr');
-
             var response = JSON.parse(this.responseText);
-            for (period in response.properties.periods) {
-                var day = response.properties.periods[period].name;
-                var temp = response.properties.periods[period].temperature + " "
-                    + response.properties.periods[period].temperatureUnit;
-                var forecastText = response.properties.periods[period].detailedForecast;
-                var icon = response.properties.periods[period].icon;
+            generateTable(response);
+        } else if (this.readyState == 4 && this.status != 200) {
+            document.getElementById("cwa").innerHTML = "Unavailable, try again later.";
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+};
 
-                header = document.createElement('th');
-                header.textContent = day;
-                trHeader.appendChild(header);
+function setCoordinatesAndCity(response) {
+    var city = response.city ? response.city : response.records[0].fields.city;
+    var coordinates = response.geopoint ? response.geopoint : response.records[0].fields.geopoint;
+    document.getElementById("city").innerHTML = "City: " + city;
+    document.getElementById("coordinates").innerHTML = "Coordinates: " + coordinates;
+    return coordinates;
+}
 
-                tdTemp = document.createElement('td');
-                tdTemp.textContent = temp;
-                trTemp.appendChild(tdTemp);
+function setStation(response) {
+    var cwa = response.cwa ? response.cwa : response.properties.cwa;
+    document.getElementById("cwa").innerHTML = "Center Weather Advisory: "
+        + cwa;
+};
 
-                tdIcon = document.createElement('td');
-                iconImg = document.createElement('img');
-                iconImg.src = icon;
-                tdIcon.appendChild(iconImg);
-                trIcon.appendChild(tdIcon);
+function generateTable(response) {
+    var periods = response.periods ? response.periods : (response.properties ? response.properties.periods: null);
+    if (!periods) {
+        // Throw error msg
+    } else {
+        table = document.createElement('table');
+        trHeader = document.createElement('tr');
+        trTemp = document.createElement('tr');
+        trIcon = document.createElement('tr');
+        for (period in periods) {
+            var day = periods[period].name;
+            var temp = periods[period].temperature + " "
+                + periods[period].temperatureUnit;
+            var forecastText = periods[period].detailedForecast;
+            var icon = periods[period].icon;
 
-                table.appendChild(trHeader);
-                table.appendChild(trTemp);
-                table.appendChild(trIcon);
+            header = document.createElement('th');
+            header.textContent = day;
+            trHeader.appendChild(header);
 
-                document.getElementsByTagName('main')[0].appendChild(table);
-            }
+            tdTemp = document.createElement('td');
+            tdTemp.textContent = temp;
+            trTemp.appendChild(tdTemp);
+
+            tdIcon = document.createElement('td');
+            iconImg = document.createElement('img');
+            iconImg.src = icon;
+            tdIcon.appendChild(iconImg);
+            trIcon.appendChild(tdIcon);
+
+            table.appendChild(trHeader);
+            table.appendChild(trTemp);
+            table.appendChild(trIcon);
+
+            document.getElementsByTagName('main')[0].appendChild(table);
+        }
+    }
+}
+
+function getWeeklyForecastBackend() {
+    document.getElementById("city").style.removeProperty("display");
+    document.getElementById("city").innerHTML = "City: Retrieving.....";
+    document.getElementById("coordinates").style.removeProperty("display");
+    document.getElementById("coordinates").innerHTML = "Coordinates: Retrieving.....";
+    document.getElementById("cwa").style.removeProperty("display");
+    document.getElementById("cwa").innerHTML = "Center Weather Advisory: Retrieving.....";
+
+    var table = document.getElementsByTagName('table')[0];
+    if (table) {
+        document.getElementById("main").removeChild(table);
+    }
+
+    var zip = document.getElementById("zipCode").value;
+
+    var url = "./weather/forecast?zip=" + zip;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            setCoordinatesAndCity(response);
+            setStation(response);
+            generateTable(response);
         } else if (this.readyState == 4 && this.status != 200) {
             document.getElementById("cwa").innerHTML = "Unavailable, try again later.";
         }
